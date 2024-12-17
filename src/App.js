496df5f34b5f8ad7f22c337c90e6216ca64c953a
xodnd0507 <24457545yong@gmail.com>
2024-12-17 23:59:47 +0900

    feat : Add router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import CardGamePage from './pages/CardGamePage';
 import RSPgamePage from './pages/RSPgamePage';
+import WatermelonGame from './pages/WatermelonGame';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/card-game" element={<CardGamePage />} />
         <Route path="/rsp-game" element={<RSPgamePage />} />
+        <Route path="/wm-game" element={<WatermelonGame />} />
       </Routes>
     </Router>
   );
